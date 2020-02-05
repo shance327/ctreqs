5a3b1606ce45bbda37a8e411c4a9f3686a31cb29
Thien <tpdoan01@louisville.edu>
2020-02-04 19:53:50 -0500

    Initial Commit

---
+++ src/main/java/com/cecs550/ctreqs/CtreqsApplication.java
@@ -0,0 +1,13 @@
+package com.cecs550.ctreqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtreqsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtreqsApplication.class, args);
+    }
+
+}
