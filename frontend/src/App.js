16cc85bf1c74c03d16123af9780c7aa54aa112eb
miguel herrera <miguel.herrera@louisville.edu>
2020-02-25 15:58:40 -0500

    Prototype components and prototype front-end application added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import TestDisplay from "./Components/TestDisplay";
+class App extends Component {
 
-function App () {
-    const [message, setMessage] = useState("");
+    state = { recipeDetails: [] }
 
-    useEffect(() => {
-        fetch('/api/hello')
-            .then(response => response.text())
-            .then(message => {
-                setMessage(message);
-            });
-    },[]);
-    return (
-        <div className="App">
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">{message}</h1>
-        </header>
-        <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-    </div>
-)
-}
+    componentDidMount() {
+        fetch('/api/recipes')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({ recipeDetails: data })
+            })
+            .catch(console.log) }
 
-export default App;
+        render() {
+            return (
+                <TestDisplay recipeDetail = {this.state.recipeDetails} />
+            )
+        }
+    }
+    export default App
+
