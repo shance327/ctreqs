43d2ce414640ce934e14f86a6e4b55213dfb8cca
shance327 <sahanc05@louisville.edu>
2020-04-21 05:19:39 -0400

    Added the ability to search for recipes (NOTE: CSS needs to be corrected, bug with being able to select more than one recipe, did not have time to fix)

--- frontend/src/Components/Pages/SearchBarPage.js
+++ frontend/src/Components/Pages/SearchBarPage.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import SearchBar from "../UI/SearchBar";
-import Button from '@material-ui/core/Button';
-import {ThemeProvider} from '@material-ui/core/styles';
-import {createMuiTheme} from "@material-ui/core";
 
 
 class SearchBarPage extends React.Component {
@@ -11,54 +8,13 @@ class SearchBarPage extends React.Component {
         ingredients: []
     };
 
-    //theme used for search buttons
-    theme = createMuiTheme({
-        palette: {
-            primary: {
-                main: '#32CD32',
-                contrastText: '#6D616F'
-            },
-            secondary: {
-                main: '#6D616F',
-                contrastText: '#32CD32'
-            },
-            contrastThreshold: 3,
-            tonalOffset: 0.2
-        }
-    });
-
-
     render() {
+        console.log(this.props)
         return(
-            <SearchBar ingredients = {this.ingredients}/>
+            <div>
+                <SearchBar ingredients = {this.ingredients}/>
+            </div>
         )
     }
-
-    handleBuzz = () => {
-            this.buzzPicks(this.state.selected);
-        };
-        // Event handler for search button
-        handleSearch = () => {
-            this.makeApiCall(this.state.selected);
-        };
-
-        buzzPicks = () => {
-            let searchUrl = `/v1/randomRecipe`;
-
-            fetch(searchUrl)
-                .then(response => {
-                    return response.json();
-                })
-                .then(jsonData => {
-                    console.log(jsonData);
-                    this.setState({ response: [...this.state.response ,jsonData] });
-                });
-            console.log(this.state.response.length);
-            console.log("search response:" + this.state.response);
-        };
-
-        searchByRecipe = () => {
-            window.location = '/TestPage';
-        };
 }
 export default SearchBarPage
--- frontend/src/Components/UI/SearchBar.js
+++ frontend/src/Components/UI/SearchBar.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Chip from '@material-ui/core/Chip';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
 import './SearchBar.css';
@@ -7,6 +6,10 @@ import { Redirect } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import {ThemeProvider} from '@material-ui/core/styles';
 import {createMuiTheme} from "@material-ui/core";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ListIcon from "@material-ui/icons/List";
+import LocalDrinkIcon from "@material-ui/icons/LocalDrink";
 
 export default class SearchBar extends Component {
 
@@ -14,7 +17,9 @@ export default class SearchBar extends Component {
     state = {
             ingredients: [],
             selected: [],
-            response: []
+            response: [],
+            recipeSearch: false,
+            recipes: []
         };
 
     // Makes a request before the first render to load ingredient information in to the options of
@@ -56,64 +61,54 @@ export default class SearchBar extends Component {
         });
     };
 
-    // Renders the search bar and or a redirect if a search response has been received.
-    render() {
-        if (this.state.response.length > 0) {
-            return (<Redirect
-                to={{
-                    pathname: "/results",
-                    state: { responses: this.state.response }
-                }}
-            />) }
-        else {
-            return (
-                <div className="SearchPage" style={{marginTop: 50}}>
-                    <h1>CTReqs</h1>
-                    <img src = "lime.jpg" style= {{ width: 300, height: 200, borderRadius: 20}}  />
-                    <p>Enter ingredients in the search bar or <a href='\TestPage'>search by recipe here</a> </p>
-                    <ThemeProvider theme ={this.theme}>
-                        <br/>
-                        <Button color ="primary" variant = "contained" className ="searchButton" onClick={this.handleSearch}>Search</Button>
-                        <br/>
-                    </ThemeProvider>
+    setRecipeSearch() {
+        this.setState({recipeSearch : true});
+        this.callRecipeInfo();
+    };
 
-                    <Autocomplete
-                        multiple
-                        size = 'medium'
-                        id="tags-outlined"
-                        options={this.state.ingredients}
-                        getOptionLabel={option => option.name}
-                        //defaultValue={}
-                        filterSelectedOptions
-                        onChange={this.onTagsChange}
-                        renderInput={params => (
-                            <TextField
-                                {...params}
-                                variant="outlined"
-                                //label="Input Ingredients"
-                                //placeholder="Ingredients"
-                            />
-                        )}
-                    />
+    setIngredientSearch() {
+        this.setState({recipeSearch : false});
+    };
 
-                    <ThemeProvider theme ={this.theme}>
-                        <Button color ="primary" variant = "contained" onClick={this.handleSearch}>Search</Button>
-                        <Button color ="secondary" variant ="contained" onClick={this.handleBuzz}>I'm Feeling Buzzed</Button>
-                    </ThemeProvider>
-                </div>
-                )
-            }
-        }
+    callRecipeInfo() {
+        let infoUrl = `/v1/recipes`;
+        fetch(infoUrl)
+            .then(response => {
+                return response.json();
+            })
+            .then(jsonData => {
+                this.setState({ recipes: jsonData });
+            });
+    };
+
     handleBuzz = () => {
         this.buzzPicks(this.state.selected);
     };
     // Event handler for search button
     handleSearch = () => {
-        this.makeApiCall(this.state.selected);
+        if (this.state.recipeSearch == true) {
+            this.recipeSearch(this.state.selected[0].recipeId);
+        }
+        else {
+            this.makeApiCall(this.state.selected);
+        }
+    };
+
+    recipeSearch = (id) => {
+
+        let searchUrl = `/v1/recipe/${id}`;
+
+        fetch(searchUrl)
+            .then(response => {
+                return response.json();
+            })
+            .then(jsonData => {
+                this.setState({ response: [...this.state.response ,jsonData] });
+            });
     };
 
     buzzPicks = () => {
-        let searchUrl = `/v1/randomRecipe`;
+        let searchUrl = `/v1/randomRecipe?id=`;
 
         fetch(searchUrl)
             .then(response => {
@@ -123,8 +118,6 @@ export default class SearchBar extends Component {
                 console.log(jsonData);
                 this.setState({ response: [...this.state.response ,jsonData] });
             });
-        console.log(this.state.response.length);
-        console.log("search response:" + this.state.response);
     };
 
     searchByRecipe = () => {
@@ -139,11 +132,11 @@ export default class SearchBar extends Component {
         for(let i = 0; i < searchInput.length; i++) {
             let obj = searchInput[i];
             ids.push(obj.id);
-            }
+        }
 
         const encodedValue = encodeURIComponent(ids);
         let searchUrl = `/v1/ingredientIds?id=${encodedValue}`;
-
+        console.log(searchUrl)
         fetch(searchUrl)
             .then(response => {
                 return response.json();
@@ -153,7 +146,108 @@ export default class SearchBar extends Component {
                 this.setState({ response: jsonData });
             });
 
-        console.log("search response:" + this.state.response);
-
     };
+
+    // Renders the search bar and or a redirect if a search response has been received.
+    render() {
+        console.log(this.state.response)
+        if (this.state.response.length > 0) {
+            return (<Redirect
+                to={{
+                    pathname: "/results",
+                    state: { responses: this.state.response }
+                }}
+            />) }
+        else {
+            if (this.state.recipeSearch == true) {
+                console.log('true')
+                    return (
+                        <div className="SearchPage" style={{marginTop: 50}}>
+                            <h1>CTReqs</h1>
+                            <img src = "lime.jpg" style= {{ width: 300, height: 200, borderRadius: 20}}  />
+                            <Autocomplete
+                                multiple
+                                size = 'medium'
+                                id="tags-outlined"
+                                limitTags={1}
+                                options={this.state.recipes}
+                                getOptionLabel={option => option.recipeName}
+                                //defaultValue={}
+                                filterSelectedOptions
+                                onChange={this.onTagsChange}
+                                renderInput={params => (
+                                    <TextField
+                                        {...params}
+                                        variant="outlined"
+                                        //label="Input Ingredients"
+                                        //placeholder="Ingredients"
+                                    />
+                                )}
+                            />
+
+                            <ThemeProvider theme ={this.theme}>
+                                <Button color ="primary" variant = "contained" onClick={this.handleSearch}>Search</Button>
+                                <Button color ="secondary" variant ="contained" onClick={this.handleBuzz}>I'm Feeling Buzzed</Button>
+                            </ThemeProvider>
+
+                            <br/>
+                            <ThemeProvider theme ={this.theme}>
+                                <ToggleButtonGroup size="large" exclusive className="toggleGroup">
+                                    <ToggleButton  key={1} value="left" onClick={() => this.setIngredientSearch()}>
+                                        <ListIcon className="toggleButton" />
+                                    </ToggleButton>,
+                                    <ToggleButton key={2} value="center"  onClick={() => this.setRecipeSearch()}>
+                                        <LocalDrinkIcon className="toggleButton" />
+                                    </ToggleButton>
+                                </ToggleButtonGroup>
+                            </ThemeProvider>
+                        </div>
+                    )
+                }
+            else{
+                console.log(this.state.ingredients)
+                return (
+                    <div className="SearchPage" style={{marginTop: 50}}>
+                        <h1>CTReqs</h1>
+                        <img src = "lime.jpg" style= {{ width: 300, height: 200, borderRadius: 20}}  />
+                        <Autocomplete
+                            multiple
+                            size = 'medium'
+                            id="tags-outlined"
+                            options={this.state.ingredients}
+                            getOptionLabel={option => option.name}
+                            //defaultValue={}
+                            filterSelectedOptions
+                            onChange={this.onTagsChange}
+                            renderInput={params => (
+                                <TextField
+                                    {...params}
+                                    variant="outlined"
+                                    //label="Input Ingredients"
+                                    //placeholder="Ingredients"
+                                />
+                            )}
+                        />
+                        <ThemeProvider theme ={this.theme}>
+                            <Button color ="primary" variant = "contained" onClick={this.handleSearch}>Search</Button>
+                            <Button color ="secondary" variant ="contained" onClick={this.handleBuzz}>I'm Feeling Buzzed</Button>
+                        </ThemeProvider>
+
+                        <br/>
+                        <ThemeProvider theme ={this.theme}>
+                            <ToggleButtonGroup size="large" exclusive className="toggleGroup">
+                                <ToggleButton className="toggleButton" key={1} value="left" onClick={() => this.setIngredientSearch()}>
+                                    <ListIcon />
+                                </ToggleButton>,
+                                <ToggleButton key={2} value="center" className="toggleButton" onClick={() => this.setRecipeSearch()}>
+                                    <LocalDrinkIcon />
+                                </ToggleButton>
+                            </ToggleButtonGroup>
+                        </ThemeProvider>
+                    </div>
+                )
+                }
+            }
+    }
+
 }
